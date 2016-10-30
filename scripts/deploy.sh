20c763ea502b72e87ccb8517ac6a4f91c5e96bc7
Akira Sosa <akirasosa@gmail.com>
2016-10-30 18:27:58 +0700

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,6 @@ set -u
 ENV=$1
 
 if [ "${ENV}" = "prod" ]; then
-  # reset current role if exists
-  test ! -v AWS_SESSION_TOKEN && direnv reload
-  # switch to production role
   source scripts/switch-production-role.sh
 fi
 
@@ -15,8 +12,3 @@ fi
 account_number=$(aws sts get-caller-identity --output text --query 'Account')
 
 sls deploy -v --stage ${ENV} | sed -e "s/${account_number}/SECRET/g"
-
-if [ "${ENV}" = "prod" ]; then
-  # reset current role if exists
-  test ! -v AWS_SESSION_TOKEN && direnv reload
-fi

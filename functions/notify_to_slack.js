18d63e3ee846640d9d58b6fe4cad4521583a8ce5
Akira Sosa <akirasosa@gmail.com>
2016-10-30 19:57:36 +0700

    parse sns event when notify it to slack

--- functions/notify_to_slack.js
+++ functions/notify_to_slack.js
@@ -4,8 +4,23 @@ const notifyToSlack = require('./notify_to_slack/index');
 
 module.exports.handle = (event, context, callback) => {
 
-  notifyToSlack(event);
+  let notification = event;
+
+  if(event.Records[0].Sns) {
+    notification = parseSnsEvent(event);
+  }
+
+  notifyToSlack(notification);
   callback(null, {});
 
 };
+
+function parseSnsEvent(event) {
+  const sns = event.Records[0].Sns;
+
+  return {
+    Subject: sns.Subject,
+    Message: JSON.parse(sns.Message),
+  }
+}
 

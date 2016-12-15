57e8a9cb087016ee10b922f3499c828928568429
Akira Sosa <akirasosa@gmail.com>
2016-12-15 09:27:56 +0700

    refactor deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,4 @@ if [ ! -v AWS_SESSION_TOKEN ]; then
   source ./scripts/switch-role.sh
 fi
 
-# account number to mask
-account_number=$(aws sts get-caller-identity --output text --query 'Account')
-
-sls deploy -v --stage ${ENV} | sed -e "s/${account_number}/SECRET/g"
+sls deploy -v --stage ${ENV}

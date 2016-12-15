4872bfd90a1badc1b18dcb2131b32ba2f718e1a7
Akira Sosa <akirasosa@gmail.com>
2016-12-15 09:26:00 +0700

    It's OK now. Schedule is rate 24 hours.

--- functions/clean_ecr_images.js
+++ functions/clean_ecr_images.js
@@ -2,6 +2,8 @@ const cleanEcrImages = require('./clean_ecr_images/index');
 
 module.exports.handle = (event, context, callback) => {
 
+  console.log(event);
+
   cleanEcrImages(event).then(() => {
     callback(null, {});
   }).catch(e => {
